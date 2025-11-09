d2f7782d7fcae456cb654e50e51cdf6a5bd1321d
Ernan <90629225+Opereirareis@users.noreply.github.com>
2025-11-09 11:55:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                // Destaques
+                'laranja-primario': '#7116AA',
+                'laranja-claro-hover': '#C084FC',
+                'cinza-complementar': '#424242',
+                // Fundos
+                'fundo-principal': '#121212',
+                'fundo-cards': '#212121',
+                // Texto
+                'branco-suave': '#E0E0E0',
+                'cinza-medio': '#BDBDBD',
+                'cinza-escuro': '#757575',
+            },
+            fontFamily: {
+                'titulos': ['Poppins', 'Montserrat', 'sans-serif'],
+                'texto-corrido': ['Inter', 'Roboto', 'sans-serif'],
+                'detalhes': ['Space Grotesk', 'monospace'],
+            },
+        }
+    }
+};
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                // Destaques
+                'laranja-primario': '#FF5722',
+                'laranja-claro-hover': '#FF7043',
+                'cinza-complementar': '#424242',
+                // Fundos
+                'fundo-principal': '#121212',
+                'fundo-cards': '#212121',
+                // Texto
+                'branco-suave': '#E0E0E0',
+                'cinza-medio': '#BDBDBD',
+                'cinza-escuro': '#757575',
+            },
+            fontFamily: {
+                'titulos': ['Poppins', 'Montserrat', 'sans-serif'],
+                'texto-corrido': ['Inter', 'Roboto', 'sans-serif'],
+                'detalhes': ['Space Grotesk', 'monospace'],
+            },
+        }
+    }
+};
